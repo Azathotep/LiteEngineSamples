e8afb262eb4f3799c7b76c0878b78190fc6347d4
Azathotep <james.sutherland2014@gmail.com>
2014-05-18 11:40:23 +0100

    Add game grid

--- Snake/Game.cs
+++ Snake/Game.cs
@@ -1,4 +1,5 @@
-﻿using LiteEngine.Rendering;
+﻿using LiteEngine.Math;
+using LiteEngine.Rendering;
 using LiteEngine.Xna;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Input;
@@ -13,16 +14,23 @@ namespace Snake
     class Game : LiteXnaEngine
     {
         Camera2D _camera;
-        
+        Snake _snake;
+        WorldGrid _grid;
+
         protected override void Initialize(XnaRenderer renderer)
         {
             renderer.SetScreenSize(1024, 768, false);
             _camera = new Camera2D(new Vector2(0, 0), new Vector2(50, 50));
+            _grid = new WorldGrid();
+
+            _snake = new Snake();
         }
 
         protected override void DrawFrame(GameTime gameTime, XnaRenderer renderer)
         {
-           
+            renderer.BeginDraw(_camera);
+            _grid.Draw(renderer);
+            renderer.EndDraw();
         }
 
         protected override void UpdateFrame(GameTime gameTime)
---
+++ Snake/WorldGrid.cs
@@ -0,0 +1,25 @@
+﻿using LiteEngine.Math;
+using LiteEngine.Rendering;
+using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+    class WorldGrid
+    {
+        SizeI _size = new SizeI(20, 20);
+
+        public void Draw(XnaRenderer renderer)
+        {
+            for (int y = 0; y < _size.Height; y++)
+                for (int x = 0; x < _size.Width; x++)
+                {
+                    renderer.DrawFilledRectangle(new RectangleF(x, y, 0.9f, 0.9f), Color.Red);
+                }
+        }
+    }
+}
